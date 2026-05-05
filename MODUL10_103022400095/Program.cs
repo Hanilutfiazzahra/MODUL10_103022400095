852a1904669b29aedc9088e82d3cea4586ea817d
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-05-06 06:56:38 +0700

    first commit

--- MODUL10_103022400095/Program.cs
+++ MODUL10_103022400095/Program.cs
@@ -1,4 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+// first commit
+// modul 10 commit
 
 // Add services to the container.
 
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+// Configure the HTTP request pipeline.
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
